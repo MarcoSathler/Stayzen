337f1c4c93fbd7a05c05b07e90df9adbef7102dd
marco.sathler <marco.sathler11@gmail.com>
2026-02-23 14:17:39 -0300

    Fix resource model

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Service extends Model
+class Resource extends Model
 {
     protected $fillable = [
         'name',
